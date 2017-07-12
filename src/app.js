ce80bcb002cd20e6c2731a2c902fbabb9313afc7
bender <tomi.kurvinen@gmail.com>
2017-07-12 16:16:05 +0300

    added input fields

--- src/app.js
+++ src/app.js
@@ -10,23 +10,43 @@ const ipfs = new IPFS({
 
 ipfs.once('ready', () => ipfs.id((err, info) => {
     if (err) { throw err }
-    console.log('IPFS now ready with address ' + info.id)
+    newMessage('<b>IPFS now ready with address ' + info.id + '</b>')
 }))
 
 const room = Room(ipfs, 'ipfs-pubsub-demo')
 
-room.on('peer joined', (peer) => console.log('peer'+ peer + ' joined'))
-room.on('peer left', (peer) => console.log('peer'+ peer + ' left'))
+function newMessage(msg) {
+    document.getElementById('message').innerHTML += '<br>' + msg
+}
+
+room.on('peer joined', (peer) => {
+    console.log('peer'+ peer + ' joined'),
+    newMessage('peer joined: ' + peer)
+})
+room.on('peer left', (peer) => {
+    console.log('peer'+ peer + ' left'),
+    newMessage('peer left: ' + peer)
+})
 
 // send and receive messages
-
-room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer + '!'))
-room.on('message', (message) => console.log('got message from ' + message.from + ': ' + message.data.toString()))
+room.on('peer joined', (peer) => {
+    room.sendTo(peer, 'Hello ' + peer + '!'),
+    newMessage('Hello ' + peer + '!')
+})
+room.on('message', (message) => {
+    console.log('got message from ' + message.from + ': ' + message.data.toString()),
+    newMessage(': ' + message.data.toString())
+})
 
 // broadcast every 2 seconds
+// setInterval(() => room.broadcast('Ralph: Im a message!'), 10000)
 
-setInterval(() => room.broadcast('hey everyone!'), 2000)
-
+// new repo initialized for each window
 function repo () {
     return 'ipfs/pubsub-demo/' + Math.random()
 }
+
+document.getElementById("postButton").onclick = function() { 
+    var newMsg = document.getElementById("postTxt").value
+    room.broadcast(newMsg)
+ }
